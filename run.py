e66744fd810f8df8ca5327ec73fb784cc084afce
Chris Styan <chrisstyan44@outlook.com>
2018-09-03 16:45:15 +0000

    Added Write to data functions and get username, still needs perfecting to obtain full functionality

--- run.py
+++ run.py
@@ -7,19 +7,33 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 usernames = []
 
+def write_to_data(filename, data):
+    with open(filename, "a") as file:
+        file.writelines(data)
+
 def add_usernames(username):
-    usernames.append("{}".format(username))
+    write_to_data("data/users.txt", "({0}) - {1}\n".format(
+            username.title()))
+
+def get_all_users():
+    users = []
+    with open("data/users.txt", "r") as user_messages:
+        users = user_messages.readlines()
+    return users
     
 
 @app.route('/')
 def riddle_me_this():
     return render_template("index.html")
     
-@app.route('/<username>')
+@app.route('/<username>', methods=["GET", "POST"])
 def user(username):
-    add_usernames(username)
-    return redirect(username)
-    return "Welcome, {0}" + username
+    
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+
+    riddle_index = 0
     
 
 if __name__ == '__main__':
